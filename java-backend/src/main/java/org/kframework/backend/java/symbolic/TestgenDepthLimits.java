e82438ca8f805cc2b9dd17555c40066b687085af
yuwen2 <laurayuwen@gmail.com>
2016-04-13 22:31:37 -0500

    add TestgenDpethLimits.java TestgenKCellEmptier.java TestgenState.java, each with minor modifications to Tom's version

---
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/TestgenDepthLimits.java
@@ -0,0 +1,120 @@
+// Copyright (c) 2013-2016 K Team. All Rights Reserved.
+package org.kframework.backend.java.symbolic;
+
+import org.kframework.backend.java.kil.Variable;
+import org.kframework.backend.java.util.Constants;
+
+import java.util.Map;
+import java.util.Set;
+
+public class TestgenDepthLimits {
+
+    /**
+     * Field used for caching the hash code
+     */
+    private transient int hashCode = Constants.NO_HASHCODE;
+
+    private final int remainingDepth;
+
+    private final String rejectName;
+
+    private final String sequenceName;
+
+    private final Set<Variable> sequenceVariables;
+
+    private final int remainingSequencing;
+
+    private final Map<String, Integer> nestingLimits;
+
+    public TestgenDepthLimits(
+            int remainingDepth,
+            String rejectName,
+            String sequenceName,
+            Set<Variable> sequenceVariables,
+            int remainingSequencing,
+            Map<String, Integer> nestingLimits) {
+        this.remainingDepth = remainingDepth;
+        this.rejectName = rejectName;
+        this.sequenceName = sequenceName;
+        this.sequenceVariables = sequenceVariables;
+        this.remainingSequencing = remainingSequencing;
+        this.nestingLimits = nestingLimits;
+    }
+
+    public int getRemainingDepth() {
+        return remainingDepth;
+    }
+
+    public String getRejectName() {
+        return rejectName;
+    }
+
+    public String getSequenceName() {
+        return sequenceName;
+    }
+
+    public Set<Variable> getSequenceVariables() {
+        return sequenceVariables;
+    }
+
+    public int getRemainingSequencing() {
+        return remainingSequencing;
+    }
+
+    public Map<String, Integer> getNestingLimits() {
+        return nestingLimits;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof TestgenDepthLimits)) {
+            return false;
+        }
+
+        TestgenDepthLimits depthLimits = (TestgenDepthLimits) object;
+        return remainingDepth == depthLimits.remainingDepth &&
+               rejectName.equals(depthLimits.rejectName) &&
+               sequenceName.equals(depthLimits.sequenceName) &&
+               sequenceVariables.equals(depthLimits.sequenceVariables) &&
+               remainingSequencing == depthLimits.remainingSequencing &&
+               nestingLimits.equals(depthLimits.nestingLimits);
+    }
+
+    @Override
+    public int hashCode() {
+        int h = hashCode;
+        if (h == Constants.NO_HASHCODE) {
+            h = 1;
+            h = h * Constants.HASH_PRIME + remainingDepth;
+            h = h * Constants.HASH_PRIME + rejectName.hashCode();
+            h = h * Constants.HASH_PRIME + sequenceName.hashCode();
+            h = h * Constants.HASH_PRIME + sequenceVariables.hashCode();
+            h = h * Constants.HASH_PRIME + remainingSequencing;
+            h = h * Constants.HASH_PRIME + nestingLimits.hashCode();
+            h = h == 0 ? 1 : h;
+            hashCode = h;
+        }
+        return h;
+    }
+
+    @Override
+    public String toString() {
+        String retval =  "Remaining depth: " + remainingDepth +
+               "\nReject Next Name: " + rejectName +
+               "\nSequence Name: " + sequenceName +
+               "\nSequence Set: {";
+        for (Variable var : sequenceVariables) {
+            retval += var + " ";
+        }
+        retval += "}\nRemaining Sequencing: " + remainingSequencing;
+        retval += "\nNesting Limits:\n";
+        for(String s : nestingLimits.keySet()) {
+            retval += "\t" + s + " -> " + nestingLimits.get(s) + "\n";
+        }
+        return retval;
+    }
+}
---
+++ java-backend/src/main/java/org/kframework/backend/java/symbolic/TestgenState.java
@@ -0,0 +1,113 @@
+// Copyright (c) 2013-2016 K Team. All Rights Reserved.
+package org.kframework.backend.java.symbolic;
+
+import org.kframework.backend.java.kil.ConstrainedTerm;
+import org.kframework.backend.java.kil.Term;
+import org.kframework.backend.java.kil.TermContext;
+import org.kframework.backend.java.kil.Variable;
+import org.kframework.backend.java.util.Constants;
+
+import java.util.Map;
+import java.util.Set;
+
+public class TestgenState {
+    /**
+     * Field used for caching the hash code
+     */
+    private transient int hashCode = Constants.NO_HASHCODE;
+
+    private final ConstrainedTerm term;
+
+    private final Map<Variable, TestgenDepthLimits> depthLimits;
+
+    //these are intentionally left out of the equals and hash methods
+    private final int remainingRuntime;
+    private final int depth;
+
+    public TestgenState(ConstrainedTerm term, Map<Variable, TestgenDepthLimits> depthLimits, int remainingRuntime, int depth) {
+        this.term = term;
+        this.depthLimits = depthLimits;
+        this.remainingRuntime = remainingRuntime;
+        this.depth = depth;
+    }
+
+    public ConstrainedTerm getConstrainedTerm() {
+        return term;
+    }
+
+    public Term getTerm() {
+        return term.term();
+    }
+
+    public TermContext getTermContext() {
+        return term.termContext();
+    }
+
+    public ConjunctiveFormula getConstraint() {
+        return term.constraint();
+    }
+
+    public Map<Variable, Term> substitution() {
+        return term.constraint().substitution();
+    }
+
+    public Map<Variable, TestgenDepthLimits> depthLimits() {
+        return depthLimits;
+    }
+
+    public TestgenDepthLimits getDepthLimit(Variable var) {
+        return depthLimits.get(var);
+    }
+
+    public Set<Variable> getDepthLimitKeys() {
+        return depthLimits.keySet();
+    }
+
+    public int getRemainingRuntime() {
+        return remainingRuntime;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+
+        if (!(object instanceof TestgenState)) {
+            return false;
+        }
+
+        TestgenState testgenState = (TestgenState) object;
+        return term.equals(testgenState.term) &&
+               depthLimits.equals(testgenState.depthLimits);
+    }
+
+    @Override
+    public int hashCode() {
+        int h = hashCode;
+        if (h == Constants.NO_HASHCODE) {
+            h = 1;
+            h = h * Constants.HASH_PRIME + term.hashCode();
+            h = h * Constants.HASH_PRIME + depthLimits.hashCode();
+            h = h == 0 ? 1 : h;
+            hashCode = h;
+        }
+        return h;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Term:\n" + term + "\n";
+        result = result + "Depth Limits:\n";
+        for(Variable var: depthLimits.keySet()) {
+            result = result + var + " -> {" + depthLimits.get(var) + "}\n";
+        }
+        result = result + "Remaining Runtime: " + remainingRuntime + "\n";
+        return result;
+    }
+
+}
